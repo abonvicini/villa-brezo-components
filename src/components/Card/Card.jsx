e56b27ca316cb619dcfd3ba933d365b32fdd380c
Alexis Bonvicini <alexisbonvicini@gmail.com>
2021-08-20 01:28:56 -0300

    create-react-app and first component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import imgHouse from "../../images/asd.jpg"
+import "./Card.css"
+
+const Card = () => {
+    return (
+    <div className="card">
+      <img className="img" src={imgHouse} alt="Imagen de una casa" />
+      <p className="card-paragraph">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos deleniti modi quia praesentium iariatur reiciendis ducimus ut aut inventore!
+      </p>
+    </div>
+  )
+}
+
+export default Card
