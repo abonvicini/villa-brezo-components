492d73d903b54749f44d0c3f94857dfdd027d5b3
Alexis Bonvicini <alexisbonvicini@gmail.com>
2021-08-22 01:28:17 -0300

    Added CardContainer to Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import imgHouse from "../../images/asd.jpg"
+import CardContainer from '../CardContainer'
 import "./Card.css"
 
 const Card = () => {
   return (
    
+    <CardContainer styleCard="card--default">
 
       <div className="card">
         <img className="img" src={imgHouse} alt="Imagen de una casa" />
@@ -12,6 +14,9 @@ const Card = () => {
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos deleniti modi quia praesentium iariatur reiciendis ducimus ut aut inventore!
         </p>
       </div>
+    
+    </CardContainer>
+
 
     
 
