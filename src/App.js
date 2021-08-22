f837266c6c248b650788aaca8eb557ff984a1077
Alexis Bonvicini <alexisbonvicini@gmail.com>
2021-08-22 01:45:12 -0300

    Css for display boths components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import Card from "./components/Card"
+import CardNews from "./components/CardNews"
 
 import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">  
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      
+      <Card/>
+      
+  
+
      </div>
+    <div className="App">  
+      
+      <CardNews/>
+      
+  
+
+     </div>
+    </>
   );
 }
 
